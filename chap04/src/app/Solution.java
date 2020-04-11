b2b8ba653a1c70d1a490ba4e49928d22941fef48
yjystyle <junyeong.joycity@gmail.com>
2020-04-11 21:09:49 +0900

    .

---
+++ chap04/src/app/Solution.java
@@ -0,0 +1,44 @@
+package app;
+
+/**
+ * Solution
+ */
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        solution(new int[]{1, 1, 9, 1, 1, 1	}, 0);
+    }
+
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+        int l = location;
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i : priorities){
+            que.add(i);
+        }
+
+        Arrays.sort(priorities);
+        int size = priorities.length-1;
+
+
+
+        while(!que.isEmpty()){
+            Integer i = que.poll();
+            if(i == priorities[size - answer]){
+                answer++;
+                l--;
+                if(l <0)
+                    break;
+            }else{
+                que.add(i);
+                l--;
+                if(l<0)
+                    l=que.size()-1;
+            }
+        }
+
+        return answer;
+    }
+}
